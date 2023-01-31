40ebce464dfecd6217835595c54bf317b3a86e2c
Seth <lajoices3611@gmail.com>
2023-01-31 12:57:47 -0600

    Allow basic functionality and stylings

--- script.js
+++ script.js
@@ -1,46 +1,103 @@
-//event listeners
-document.querySelectorAll("button").forEach(function (element) {
-  element.addEventListener("click", findValue);
+class Calculator {
+  constructor(previousOppElement, currentOppElement) {
+    this.previousOppElement = previousOppElement;
+    this.currentOppElement = currentOppElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if(this.currentOperand === '') return;
+    if(this.previousOperand !== '') {
+        this.compute()
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand
+    this. currentOperand = '';
+  }
+
+  compute() {
+    let result
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+        case "+":
+            result = prev + current;
+            break;
+        case "-":
+            result = prev - current;
+            break;
+        case "*":
+            result = prev * current;
+            break;
+        case "/":
+            result = (prev / current);
+            break;
+            default:
+                return;
+    }
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = '';
+}
+
+  updateDisplay() {
+    this.currentOppElement.innerText = this.currentOperand;
+    if(this.operation != null) {
+        this.previousOppElement.innerText = 
+        `${this.previousOperand} ${this.operation}`
+    }
+    this.previousOppElement.innerText = this.previousOperand;
+  }
+}
+const numberButtons = document.querySelectorAll("[data-number]");
+const oppButtons = document.querySelectorAll("[data-operator]");
+const clearButton = document.querySelector("#oppClear");
+const deleteButton = document.querySelector("#oppDel");
+const equalButton = document.querySelector("#oppEnter");
+const previousOppElement = document.querySelector("#previous-opp");
+const currentOppElement = document.querySelector("#current-opp");
+
+const calculator = new Calculator(previousOppElement, currentOppElement);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
 });
 
-function add(a, b) {
-  display(a + b);
-}
+oppButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-function subtract(a, b) {
-  display(a - b);
-}
+equalButton.addEventListener("click", button =>{
+    calculator.compute();
+    calculator.updateDisplay();
+})
 
-function multiply(a, b) {
-  display(a * b);
-}
+clearButton.addEventListener("click", button =>{
+    calculator.clear();
+    calculator.updateDisplay();
+})
 
-function divide(a, b) {
-  display(a / b);
-}
-
-function findValue() {
-  return buttonValue = this.textContent || this.innerText;
-}
-
-function operate(operator, num1, num2) {
-    switch(operator) {
-        case "+":
-            add(num1, num2);
-            break;
-        case "-" :
-            subtract(num1,num2);
-            break;
-        case "*" :
-            multiply(num1,num2);
-            break;
-        case "/" :
-            divide(num1, num2);
-            break;
-    } 
-}
-
-function display(equation) {
-  let screen = document.querySelector("#displayScreen");
-  return (screen.innerHTML = equation);
-}
+deleteButton.addEventListener("click", button =>{
+    calculator.delete();
+    calculator.updateDisplay();
+})
